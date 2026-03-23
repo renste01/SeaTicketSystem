70712d7441f4cf124fb0e27f40152e2405e7bb58
rekast <renste01@easv365.dk>
2026-03-23 12:54:30 +0100

    DB Connector

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module dk.easv.seaticketsystem {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires com.microsoft.sqlserver.jdbc;
 
 
     opens dk.easv.seaticketsystem to javafx.fxml;
