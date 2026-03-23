85003885761d44259f60b955732bdffcf90917eb
rekast <renste01@easv365.dk>
2026-03-23 16:39:54 +0100

    Added small comments

--- src/main/java/dk/easv/seaticketsystem/BLL/UserService.java
+++ src/main/java/dk/easv/seaticketsystem/BLL/UserService.java
@@ -1,21 +1,21 @@
 package dk.easv.seaticketsystem.BLL;
-
+// Project Imports
 import dk.easv.seaticketsystem.Model.Admin;
 import dk.easv.seaticketsystem.Model.EventCoordinator;
 import dk.easv.seaticketsystem.Model.User;
 import dk.easv.seaticketsystem.Model.UserRole;
 
+// Java Imports
 import java.util.List;
 import java.util.Optional;
 
-public class UserService {
+public class UserService
+{
 
-    private final List<User> staffUsers = List.of(
-            new Admin("admin-001", "System", "Administrator", "admin@sea.dk", "admin123"),
-            new EventCoordinator("coord-001", "Lars", "Nielsen", "koordinator@sea.dk", "koor123")
-    );
+    private final List<User> staffUsers = List.of(new Admin("admin-001", "System", "Administrator", "admin@sea.dk", "admin123"), new EventCoordinator("coord-001", "Lars", "Nielsen", "koordinator@sea.dk", "koor123"));
 
-    public Optional<User> authenticateStaff(String email, String password) {
+    public Optional<User> authenticateStaff(String email, String password)
+    {
         return staffUsers.stream()
                 .filter(u -> u.getEmail().equalsIgnoreCase(email))
                 .filter(u -> u.checkPassword(password))
